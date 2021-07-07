d93a6105e58db0f98084f864d4011ba7adc3aea1
vamsi <vamsi@gamil.com>
2021-07-07 05:40:02 +0000

    second commit

--- array.sh
+++ array.sh
@@ -5,3 +5,14 @@ echo ${arr[*]}  # list of all elements in the list
 echo ${arr[1]} #getting the first element
 echo ${#arr}  #length of array
 echo ${#arr[1]} #length of first element
+# array iteration
+for i in ${arr[@]}
+do
+   echo $i
+done
+
+#to print elements with in range
+#arr:startindex:endingindex
+echo ${arr[@]:1:4}
+#arr[element index]: start index: ending index
+echo ${arr[1]:1:3}
